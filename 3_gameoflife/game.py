f532a7b0778c95e7869ef925c3a265879d96d1a9
abhi <abhi@mailbox.org>
2017-01-05 11:54:01 +0100

    Getting started with Conway's Game of Life - basic rules

---
+++ 3_gameoflife/game.py
@@ -0,0 +1,5 @@
+# Rules of Conway's game of life:
+# 1. If a cell is ON and has fewer than 2 ON neighbors, it turns OFF
+# 2. If a cell is ON and has 2 or 3 ON neighbors, it remains ON
+# 3. If a cell is ON and has >3 ON neighbors, it turns OFF
+# 4. If a cell is OFF and has 3 ON neighbors, it turns ON
